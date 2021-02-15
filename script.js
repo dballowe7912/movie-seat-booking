45b5b329beae0eded3bf669fe675d0a2d1164a35
Donald Ballowe <dballowe7912@gmail.com>
2021-02-15 04:34:27 -0600

    Added populate ui function to add local storage data on page load

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
@@ -29,6 +31,25 @@ function updateSelectedCount() {
     total.innerText = selectedSeatCount * ticketPrice;
 }
 
+// Get data from storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Event Listeners
 
 // Movie Select Event 
@@ -49,3 +70,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
