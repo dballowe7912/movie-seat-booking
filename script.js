a900ced018a7710e497527c74bb32b78dd4bcbaa
Donald Ballowe <dballowe7912@gmail.com>
2021-02-14 23:53:22 -0600

    Added selectors and event listeners to js

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    total.innerText = selectedSeatCount * ticketPrice;
+}
+
+// Event Listeners
+
+// Movie Select Event 
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+// Seat Click Event
+container.addEventListener('click', (e) => {
+    if (
+        e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')
+    ) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
